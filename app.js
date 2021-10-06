3badb00eefeccfeaa36d472d8d30021ce89f963c
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-10-06 23:53:43 +0900

    get response

--- app.js
+++ app.js
@@ -6,62 +6,62 @@
 // // 2. Install the Node.js client library by running
 // //    `npm install googleapis --save`
 
-// const {google} = require('googleapis');
-// const sheets = google.sheets('v4');
+const {google} = require('googleapis');
+const sheets = google.sheets('v4');
 
-// async function main () {
-//   const authClient = await authorize();
-//   const request = {
-//     // The ID of the spreadsheet to retrieve data from.
-//     spreadsheetId: '1VJFDkHGq8O8aJpdAZFOwwhhLBUycvdn59jAYbTnoUNA',  // TODO: Update placeholder value.
+async function main () {
+  const authClient = await authorize();
+  const request = {
+    // The ID of the spreadsheet to retrieve data from.
+    spreadsheetId: '1VJFDkHGq8O8aJpdAZFOwwhhLBUycvdn59jAYbTnoUNA',  // TODO: Update placeholder value.
 
-//     // The A1 notation of the values to retrieve.
-//     range: 'A1:B1',  // TODO: Update placeholder value.
+    // The A1 notation of the values to retrieve.
+    range: 'A1:B1',  // TODO: Update placeholder value.
 
-//     // How values should be represented in the output.
-//     // The default render option is ValueRenderOption.FORMATTED_VALUE.
-//     valueRenderOption: '',  // TODO: Update placeholder value.
+    // How values should be represented in the output.
+    // The default render option is ValueRenderOption.FORMATTED_VALUE.
+    valueRenderOption: '',  // TODO: Update placeholder value.
 
-//     // How dates, times, and durations should be represented in the output.
-//     // This is ignored if value_render_option is
-//     // FORMATTED_VALUE.
-//     // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
-//     dateTimeRenderOption: '',  // TODO: Update placeholder value.
+    // How dates, times, and durations should be represented in the output.
+    // This is ignored if value_render_option is
+    // FORMATTED_VALUE.
+    // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
+    dateTimeRenderOption: '',  // TODO: Update placeholder value.
 
-//     auth: authClient,
-//   };
+    auth: authClient,
+  };
 
-//   try {
-//     const response = (await sheets.spreadsheets.values.get(request)).data;
-//     // TODO: Change code below to process the `response` object:
-//     console.log(JSON.stringify(response, null, 2));
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
-// main();
+  try {
+    const response = (await sheets.spreadsheets.values.get(request)).data;
+    // TODO: Change code below to process the `response` object:
+    console.log(JSON.stringify(response, null, 2));
+  } catch (err) {
+    console.error(err);
+  }
+}
+main();
 
-// async function authorize() {
-//   // TODO: Change placeholder below to generate authentication credentials. See
-//   // https://developers.google.com/sheets/quickstart/nodejs#step_3_set_up_the_sample
-//   //
-//   // Authorize using one of the following scopes:
-//   //   'https://www.googleapis.com/auth/drive'
-//   //   'https://www.googleapis.com/auth/drive.file'
-//   //   'https://www.googleapis.com/auth/drive.readonly'
-//   //   'https://www.googleapis.com/auth/spreadsheets'
-//   //   'https://www.googleapis.com/auth/spreadsheets.readonly'
-//   let authClient = null;
+async function authorize() {
+  // TODO: Change placeholder below to generate authentication credentials. See
+  // https://developers.google.com/sheets/quickstart/nodejs#step_3_set_up_the_sample
+  //
+  // Authorize using one of the following scopes:
+  //   'https://www.googleapis.com/auth/drive'
+  //   'https://www.googleapis.com/auth/drive.file'
+  //   'https://www.googleapis.com/auth/drive.readonly'
+  //   'https://www.googleapis.com/auth/spreadsheets'
+  //   'https://www.googleapis.com/auth/spreadsheets.readonly'
+  let authClient = null;
 
-//   if (authClient == null) {
-//     throw Error('authentication failed');
-//   }
+  if (authClient == null) {
+    throw Error('authentication failed');
+  }
 
-//   return authClient;
-// }
+  return authClient;
+}
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer(function(req, res) {
-  res.end('connected');
-}).listen(3600)
+// const server = http.createServer(function(req, res) {
+//   res.end('connected');
+// }).listen(3600)
