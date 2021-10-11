8618f2765789e935ca7104e75cb7ba0e6b38241f
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-10-11 23:42:40 +0900

    make up create sheet api

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', async (req, res) => {
 
   const request = {
     spreadsheetId,
-    range: 'A1:D2',
+    range: 'A1:E2',
   }
 
   try {
@@ -29,7 +29,57 @@ app.get('/', async (req, res) => {
   } catch (err) {
     console.error(err);
   }
-
 })
+
+app.get('/create', async (req, res) => {
+  const auth = new docs.auth.GoogleAuth({
+    keyFile: 'credentials.json',
+    scopes: 'https://www.googleapis.com/auth/spreadsheets',
+  });
+  
+  const client = await auth.getClient();
+
+  const googleSheets = google.sheets({version: 'v4', auth: client });
+
+  const spreadsheetId = '1VJFDkHGq8O8aJpdAZFOwwhhLBUycvdn59jAYbTnoUNA';
+
+  const request_body = {
+    spreadsheetId,
+    resource: {
+      requests: [
+        {
+          "addSheet": {
+            "properties": {
+              "title": "test2"
+            }
+          }
+        }
+      ]
+    }
+  }
+
+  try {
+    const response = (await googleSheets.spreadsheets.batchUpdate(request_body)).data;
+
+    console.log(JSON.stringify(response, null, 2));
+    res.send(response.values);
+  } catch (error) {
+    console.error(error);
+  }
+})
+
+// barchUpdate使えば新しいシート作成できる
+// https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/batchUpdate?apix_params=%7B%22spreadsheetId%22%3A%221VJFDkHGq8O8aJpdAZFOwwhhLBUycvdn59jAYbTnoUNA%22%2C%22resource%22%3A%7B%22requests%22%3A%5B%7B%22addSheet%22%3A%7B%22properties%22%3A%7B%22title%22%3A%22test%22%7D%7D%7D%5D%7D%7D
+//{
+//   "requests": [
+//     {
+//       "addSheet": {
+//         "properties": {
+//           "title": "test"
+//         }
+//       }
+//     }
+//   ]
+// }
 
 app.listen(3600, (req, res) => console.log("running on 3600"))
